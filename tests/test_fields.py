0ed764aeca9f835dc8ad12313ef4fe88eda6f857
Jakub Kulík <kulikjak@gmail.com>
2025-06-05 09:59:20 +0200

    we need the pytz condition

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1643,15 +1643,16 @@ class TestPytzNaiveDayLightSavingTimeTimeZoneDateTimeField(FieldValues):
     }
     outputs = {}
 
-    class MockTimezone(pytz.BaseTzInfo):
-        @staticmethod
-        def localize(value, is_dst):
-            raise pytz.InvalidTimeError()
+    if pytz:
+        class MockTimezone(pytz.BaseTzInfo):
+            @staticmethod
+            def localize(value, is_dst):
+                raise pytz.InvalidTimeError()
 
-        def __str__(self):
-            return 'America/New_York'
+            def __str__(self):
+                return 'America/New_York'
 
-    field = serializers.DateTimeField(default_timezone=MockTimezone())
+        field = serializers.DateTimeField(default_timezone=MockTimezone())
 
 
 @patch('rest_framework.utils.timezone.datetime_ambiguous', return_value=True)
